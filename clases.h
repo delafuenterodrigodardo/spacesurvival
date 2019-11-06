1423324ba61bf57c013c9eefef6f7922f785990d
delafuenterodrigodardo <55667663+delafuenterodrigodardo@users.noreply.github.com>
2019-11-06 20:43:44 -0300

    Add files via upload

---
+++ clases.h
@@ -0,0 +1,177 @@
+#ifndef CLASES_H_INCLUDED
+#define CLASES_H_INCLUDED
+
+const int anchoPantalla = 1600;
+const int altoPantalla = 1000;
+
+float GRADOSARADIANES = 0.017453f;
+
+
+
+class Animacion
+{
+   public:
+   float Frame, velocidad;
+   Sprite sprite;
+   std::vector<IntRect> cuadros;
+
+   Animacion(){}
+
+   Animacion (Texture &t, int x, int y, int ancho, int alto, int count, float Vel)
+   {
+     Frame = 0;
+     velocidad = Vel;
+
+     for (int i=0;i<count;i++)
+      cuadros.push_back( IntRect(x+i*ancho, y, ancho, alto)  );
+
+     sprite.setTexture(t);
+     sprite.setOrigin(ancho/2,alto/2);
+     sprite.setTextureRect(cuadros[0]);
+   }
+
+
+   void update()
+   {
+     Frame += velocidad;
+     int n = cuadros.size();
+     if (Frame >= n) Frame -= n;
+     if (n>0) sprite.setTextureRect( cuadros[int(Frame)] );
+   }
+
+   bool isEnd()
+   {
+     return Frame+velocidad>=cuadros.size();
+   }
+
+};
+
+
+class Entidad
+{
+   public:
+   float x,y,dx,dy,R,angle;
+   bool life;
+   std::string name;
+   Animacion anim;
+
+   Entidad()
+   {
+     life=10;
+   }
+
+   void settings(Animacion &a,int X,int Y,float Angle=0,int radius=1)
+   {
+     anim = a;
+     x=X; y=Y;
+     angle = Angle;
+     R = radius;
+   }
+
+   virtual void update(){};
+
+   void draw(RenderWindow &app)
+   {
+     anim.sprite.setPosition(x,y);
+     anim.sprite.setRotation(angle+90);
+     app.draw(anim.sprite);
+
+     CircleShape circle(R);
+     circle.setFillColor(Color(255,0,0,170));
+     circle.setPosition(x,y);
+     circle.setOrigin(R,R);
+     //app.draw(circle);
+   }
+
+   virtual ~Entidad(){};
+};
+
+
+class asteroide: public Entidad
+{
+   public:
+   asteroide()
+   {
+     dx=rand()%8-4;
+     dy=rand()%8-4;
+     name="asteroide";
+   }
+
+   void update()
+   {
+     x+=dx;
+     y+=dy;
+
+     if (x>anchoPantalla) x=0;  if (x<0) x=anchoPantalla;
+     if (y>altoPantalla) y=0;  if (y<0) y=altoPantalla;
+   }
+
+};
+
+
+class bala: public Entidad
+{
+   public:
+   bala()
+   {
+     name="bala";
+   }
+
+   void  update()
+   {
+     dx=cos(angle*GRADOSARADIANES)*6;
+     dy=sin(angle*GRADOSARADIANES)*6;
+     // angle+=rand()%7-3;  /*try this*/
+     x+=dx;
+     y+=dy;
+
+     if (x>anchoPantalla || x<0 || y>altoPantalla || y<0) life=0;
+   }
+
+};
+
+
+class player: public Entidad
+{
+   public:
+   bool thrust;
+
+   player()
+   {
+     name="player";
+   }
+
+   void update()
+   {
+     if (thrust)
+      { dx+=cos(angle*GRADOSARADIANES)*0.2;
+        dy+=sin(angle*GRADOSARADIANES)*0.2; }
+     else
+      { dx*=0.99;
+        dy*=0.99; }
+
+    int maxSpeed=15;
+    float velocidad = sqrt(dx*dx+dy*dy);
+    if (velocidad>maxSpeed)
+     { dx *= maxSpeed/velocidad;
+       dy *= maxSpeed/velocidad; }
+
+    x+=dx;
+    y+=dy;
+
+    if (x>anchoPantalla) x=0; if (x<0) x=anchoPantalla;
+    if (y>altoPantalla) y=0; if (y<0) y=altoPantalla;
+   }
+
+};
+
+
+bool isCollide(Entidad *a,Entidad *b)
+{
+  return (b->x - a->x)*(b->x - a->x)+
+         (b->y - a->y)*(b->y - a->y)<
+         (a->R + b->R)*(a->R + b->R);
+}
+
+
+#endif // CLASES_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,151 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <list>
+using namespace sf;
+#include "clases.h"
+
+
+
+
+int main()
+{
+    srand(time(0));
+
+    RenderWindow app(VideoMode(anchoPantalla, altoPantalla), "Space Survival");
+    app.setFramerateLimit(60);
+
+    Texture t1,t2,t3,t4,t5,t6,t7;
+    t1.loadFromFile("images/spaceship1.png");
+    t2.loadFromFile("images/background2.jpg");
+    t3.loadFromFile("images/explosions/type_C.png");
+    t4.loadFromFile("images/rock.png");
+    t5.loadFromFile("images/fire_red.png");
+    t6.loadFromFile("images/rock_small.png");
+    t7.loadFromFile("images/explosions/type_B.png");
+
+    t1.setSmooth(true);
+    t2.setSmooth(true);
+
+    Sprite background(t2);
+
+    Animacion sExplosion(t3, 0,0,256,256, 48, 0.5);
+    Animacion sRock(t4, 0,0,64,64, 16, 0.2);
+    Animacion sRock_small(t6, 0,0,64,64, 16, 0.2);
+    Animacion sBullet(t5, 0,0,32,64, 16, 0.8);
+    Animacion sPlayer(t1, 40,0,40,40, 1, 0);
+    Animacion sPlayer_go(t1, 40,40,40,40, 1, 0);
+    Animacion sExplosion_ship(t7, 0,0,192,192, 64, 0.5);
+
+
+    std::list<Entidad*> entidades;
+
+    for(int i=0;i<15;i++)
+    {
+      asteroide *a = new asteroide();
+      a->settings(sRock, rand()%anchoPantalla, rand()%altoPantalla, rand()%360, 25);
+      entidades.push_back(a);
+    }
+
+    player *p = new player();
+    p->settings(sPlayer,200,200,0,20);
+    entidades.push_back(p);
+
+    /////main loop/////
+    while (app.isOpen())
+    {
+        Event event;
+        while (app.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                app.close();
+
+            if (event.type == Event::KeyPressed)
+             if (event.key.code == Keyboard::Space)
+              {
+                bala *b = new bala();
+                b->settings(sBullet,p->x,p->y,p->angle,10);
+                entidades.push_back(b);
+              }
+        }
+
+    if (Keyboard::isKeyPressed(Keyboard::Right)) p->angle+=3;
+    if (Keyboard::isKeyPressed(Keyboard::Left))  p->angle-=3;
+    if (Keyboard::isKeyPressed(Keyboard::Up)) p->thrust=true;
+    else p->thrust=false;
+
+
+    for(auto a:entidades)
+     for(auto b:entidades)
+     {
+      if (a->name=="asteroide" && b->name=="bala")
+       if ( isCollide(a,b) )
+           {
+            a->life=false;
+            b->life=false;
+
+            Entidad *e = new Entidad();
+            e->settings(sExplosion,a->x,a->y);
+            e->name="explosion";
+            entidades.push_back(e);
+
+
+            for(int i=0;i<2;i++)
+            {
+             if (a->R==15) continue;
+             Entidad *e = new asteroide();
+             e->settings(sRock_small,a->x,a->y,rand()%360,15);
+             entidades.push_back(e);
+            }
+
+           }
+
+      if (a->name=="player" && b->name=="asteroide")
+       if ( isCollide(a,b) )
+           {
+            b->life=false;
+
+            Entidad *e = new Entidad();
+            e->settings(sExplosion_ship,a->x,a->y);
+            e->name="explosion";
+            entidades.push_back(e);
+
+            p->settings(sPlayer,anchoPantalla/2,altoPantalla/2,0,20);
+            p->dx=0; p->dy=0;
+           }
+     }
+
+
+    if (p->thrust)  p->anim = sPlayer_go;
+    else   p->anim = sPlayer;
+
+
+    for(auto e:entidades)
+     if (e->name=="explosion")
+      if (e->anim.isEnd()) e->life=0;
+
+    if (rand()%150==0)
+     {
+       asteroide *a = new asteroide();
+       a->settings(sRock, 0,rand()%altoPantalla, rand()%360, 25);
+       entidades.push_back(a);
+     }
+
+    for(auto i=entidades.begin();i!=entidades.end();)
+    {
+      Entidad *e = *i;
+
+      e->update();
+      e->anim.update();
+
+      if (e->life==false) {i=entidades.erase(i); delete e;}
+      else i++;
+    }
+
+   //////draw//////
+   app.draw(background);
+   for(auto i:entidades) i->draw(app);
+   app.display();
+    }
+
+    return 0;
+}
